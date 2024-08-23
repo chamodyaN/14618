4626985ee81428b8701b85ecdce04529947d1482
chamodyaN <chamodyanfernando@gmail.com>
2024-08-23 11:00:49 +0530

    Add files via upload

---
+++ LightOnCommand.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class LightOnCommand implements Command{
+    private Light light;
+    public LightOnCommand(Light light){
+        this.light=light;
+    }
+    @Override
+    public void undo() {
+        light.off();
+
+    }
+    @Override
+    public void execute() {
+        light.on();
+
+    }
+}
