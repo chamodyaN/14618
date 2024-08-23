153a02cd515fe10e3ee9216ff90506aa35795a79
chamodyaN <chamodyanfernando@gmail.com>
2024-08-23 11:03:59 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Circle implements Shape{
+    @Override
+    public void draw() {
+
+        int radius = 5;
+        int r = radius;
+        int diameter = 2 * r + 1;
+        for (int i = 0; i < diameter; i++) {
+            for (int j = 0; j < diameter; j++) {
+                int x = i - r;
+                int y = j - r;
+                if (x * x + y * y <= r * r + r * 0.8) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
