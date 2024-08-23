2cf4ed59835ec63d156f97ab7f850f83c841830f
chamodyaN <chamodyanfernando@gmail.com>
2024-08-23 11:03:26 +0530

    Add files via upload

---
+++ Tea.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Tea extends Beverages {
+    @Override
+    protected void brew() {
+        System.out.println("Steeping the tea");
+    }
+
+    @Override
+    protected void addCondiments() {
+        System.out.println("Adding lemon");
+    }
+
+    @Override
+    protected void addExtras() {
+        System.out.println("Adding honey");
+    }
+}
